a413a33c0c3cc1dea7711895b70720d389e4c404
jebi2420 <jebi2420@gmail.com>
2024-04-04 16:22:37 +0900

    [add] route 적용, 페이지 생성

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
+import { Link, Routes, Route } from 'react-router-dom';
 import './App.css';
+import Homepage from './pages/Homepage';
+import ReactQueryPage from './pages/ReactQueryPage';
 
 function App() {
   return (
     <div>
+      <nav style={{backgroundColor: "pink", padding:"20px"}}>
+        <Link to="/" style={{marginRight: "10px"}}>
+          Homepage
+        </Link>
+        <Link to="/react-query">React Query</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/react-query" element={<ReactQueryPage />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/pages/ReactQueryPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ReactQueryPage = () => {
+  return (
+    <div>
+      ReactQueryPage
+    </div>
+  )
+}
+
+export default ReactQueryPage
