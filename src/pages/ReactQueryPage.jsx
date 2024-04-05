1c5bb6417677b661e8268536321bb9372ad275ef
jebi2420 <jebi2420@gmail.com>
2024-04-05 17:21:40 +0900

    [add] 디테일 페이지 불러오기

--- src/pages/ReactQueryPage.jsx
+++ src/pages/ReactQueryPage.jsx
@@ -4,12 +4,13 @@ import React from 'react'
 
 const ReactQueryPage = () => {
 
-  const fetchPost=()=>{
-    return axios.get('http://localhost:3004/posts')
+  const fetchPost=(queryData)=>{
+    const id = queryData.queryKey[1];
+    return axios.get(`http://localhost:3004/posts/${id}`)
   }
 
-  const {isLoading, data, isError, error} = useQuery({
-    queryKey:['posts'],
+  const {isLoading, data, isError, error, refetch} = useQuery({
+    queryKey:['posts', 1],
     queryFn: fetchPost,
     retry: 1,
     select:(data)=>{
@@ -20,9 +21,8 @@ const ReactQueryPage = () => {
     // //refetchInterval: 3000, // 3초마다 실시간으로 계속 호출
     // refetchOnMount: true, // 컴포넌트 들어왔을 때 fetch 할건지, 기본값 true
     // refetchOnWindowFocus: true, // 윈도우 focus 시 자동 fetch
-    
+    enabled: false, // 초기에 호출 안되게 함
   })
-
   console.log("data: " +  isLoading + " " +  JSON.stringify(data, null, "\t")); 
   console.log("error: " + isError + " " + error)
 
@@ -38,9 +38,10 @@ const ReactQueryPage = () => {
   return (
     <div>
       ReactQueryPage
-      {data.map((item)=>(
+      {data?.map((item)=>( // data가 있으면 들고와라
         <div>{item.title}</div> // data의 data에서 title만 뽑아서 보여준다
       ))}
+      <button onClick={refetch}>post리스트 다시 들고오기</button>
     </div>
   )
 }
